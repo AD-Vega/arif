d41b7b79ead4f9fe97549d743a9163ad90e35d43
Jure Varlec <jure.varlec@ad-vega.si>
2014-03-11 11:20:17 +0100

    Video source interface.

---
+++ src/videosources/interfaces.cpp
@@ -0,0 +1,55 @@
+/*
+    arif, ADV Realtime Image Filtering, a tool for amateur astronomy.
+    Copyright (C) 2014 Jure Varlec <jure.varlec@ad-vega.si>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "videosources/interfaces.h"
+
+FrameMetaData::serialize(QDataStream& s)
+{
+  s << timestamp;
+  s << frameOfSecond;
+}
+
+void FrameMetaData::load(QDataStream& s)
+{
+    s >> timestamp;
+    s >> frameOfSecond;
+}
+
+void RawFrame::serialize(QDataStream& s)
+{
+    metaData.serialize(s);
+}
+
+void RawFrame::load(QDataStream& s)
+{
+    metaData.load(s);
+}
+
+FrameMetaData Reader::makeMetaData()
+{
+    auto now = QDateTime::currentDateTimeUtc();
+    auto unixtime = now.toTime_t();
+    if (unixtime != previousUnixtime) {
+        previousUnixtime = unixtime;
+        frameOfSecond = 0;
+    }
+    FrameMetaData metadata;
+    metadata.timestamp = now;
+    metadata.frameOfSecond = ++frameOfSecond;
+    return metadata;
+}
--- src/videosources/interfaces.h
+++ src/videosources/interfaces.h
@@ -19,9 +19,109 @@
 #ifndef VIDEOSOURCES_INTERFACES_H
 #define VIDEOSOURCES_INTERFACES_H
 
+#include <QtPlugin>
+#include <QMetaType>
+#include <QSharedPointer>
+#include <QDateTime>
+#include <QGroupBox>
+#include <opencv2/core/core.hpp>
+
 // STUBS
-struct RawFrame {};
-struct Decoder {};
 struct Histograms {};
+
+class VideoSourcePlugin;
+class RawFrame;
+class Decoder;
+typedef QSharedPointer<RawFrame> SharedRawFrame;
+typedef QSharedPointer<Decoder> SharedDecoder;
+
+struct FrameMetaData
+{
+    serialize(QDataStream& s);
+    void load(QDataStream& s);
+
+    QDateTime timestamp;
+    unsigned int frameOfSecond = 0;
+};
+
+class RawFrame
+{
+public:
+    virtual ~RawFrame() {}
+    virtual SharedRawFrame copy() = 0;
+    virtual VideoSourcePlugin* format() = 0;
+
+    // Default serialization takes care of metadata.
+    virtual void serialize(QDataStream& s);
+    virtual void load(QDataStream& s);
+    FrameMetaData metaData;
+};
+
+class Decoder
+{
+public:
+    virtual ~Decoder() {}
+    virtual cv::Mat decode(RawFrame* in) = 0;
+    virtual VideoSourcePlugin* format() = 0;
+};
+
+class Reader: public QObject
+{
+    Q_OBJECT
+
+public:
+    virtual ~Reader() {}
+    virtual bool seek(qint64 frame) = 0;
+    virtual bool isSequential() = 0;
+    virtual quint64 numberOfFrames() = 0;
+    virtual VideoSourcePlugin* format() = 0;
+
+private:
+    FrameMetaData makeMetaData();
+
+public slots:
+    // Used by non-live sources to throttle reading.
+    // Live sources ignore this and emit frameReady()
+    // for every frame.
+    virtual void readFrame() = 0;
+
+signals:
+    void error(QString msg);
+    void atEnd();
+    void frameReady(SharedRawFrame frame);
+
+private:
+    uint previousUnixtime = 0;
+    uint frameOfSecond = 0;
+};
+
+class VideoSourceConfigurationWidget : public QGroupBox
+{
+    Q_OBJECT
+
+public:
+    explicit VideoSourceConfigurationWidget(const QString& title,
+                                            QWidget* parent = 0) :
+        QGroupBox(title, parent) {}
+
+signals:
+    void configurationComplete();
+};
+
+class VideoFormat
+{
+public:
+    virtual ~VideoFormat() {}
+    virtual QString name() = 0;
+    virtual VideoSourceConfigurationWidget* createConfigurationWidget() = 0;
+    virtual SharedRawFrame createRawFrame() = 0;
+    virtual SharedDecoder createDecoder() = 0;
+    virtual Reader* reader() = 0;
+    virtual QSize frameSize() = 0;
+};
+
+Q_DECLARE_INTERFACE(VideoSourcePlugin,
+                    "si.ad-vega.arif.VideoSourcePlugin/0.1");
+Q_DECLARE_METATYPE(VideoSourcePlugin*)
 
 #endif
